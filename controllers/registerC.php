42e9a498460431f8ee656bdc8b562c7e2c73cf7a
youssef <ll5575982>
2024-01-02 10:23:22 +0100

    update

--- controllers/registerC.php
+++ controllers/registerC.php
@@ -14,7 +14,6 @@ if (isset($_POST["signup"])) {
  
     $password_hash = password_hash($repeat_password, PASSWORD_DEFAULT);
     $errors = array();
-    git 
     $patternEmail = '/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/';
     $patternName ='/^[a-zA-Z\s\'.-]+$/';
     $patternPassword = '/^.{4,}$/';
