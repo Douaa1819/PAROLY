b53e985e49c4b47c77682a02128a04117da820a8
Aya <ayaelrhayour123@gmail.com>
2024-01-04 09:10:40 +0100

    hopefully the errors are fixed now

---
+++ app/views/inc/SideBar.php
@@ -0,0 +1,86 @@
+    <!-- =============== Navigation ================ -->
+    <div class="dahbord">
+        <div class="navigation">
+            <ul style="padding-left: 0rem;">
+                <li>
+                    <a href="<?php echo URLROOT; ?>">
+                        <span class="icon">
+                            <img src="<?php echo URLROOT; ?>/image/image-removebg-preview.png" class="logo" width="60px"
+                                height="80px" />
+                        </span>
+
+                        <span class="title">PAROLY</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="<?php echo URLROOT; ?>/DashbordControler">
+                        <span class="icon">
+                            <ion-icon name="home-outline"></ion-icon>
+                        </span>
+                        <span class="title">Dashboard</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="<?php echo URLROOT; ?>/DashbordControler/Genre">
+                        <span class="icon">
+                            <ion-icon name="home-outline"></ion-icon>
+                        </span>
+                        <span class="title">Genre</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="<?php echo URLROOT; ?>/DashbordControler/Album">
+                        <span class="icon">
+                        <ion-icon name="albums-outline"></ion-icon>
+                        </span>
+                        <span class="title">Album</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="Artiste.php">
+                        <span class="icon">
+                            <ion-icon name="person-outline"></ion-icon>
+                        </span>
+                        <span class="title">Artiste</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="Playliste.php">
+                        <span class="icon">
+                            <ion-icon name="invert-mode-outline"></ion-icon>
+                        </span>
+                        <span class="title">Playltiste</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="song.php">
+                        <span class="icon">
+                            <ion-icon name="musical-note-outline"></ion-icon>
+                        </span>
+                        <span class="title">Song</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="Lyrics.php">
+                        <span class="icon">
+                            <ion-icon name="mic-circle-outline"></ion-icon>
+                        </span>
+                        <span class="title">Lyrics
+                        </span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#">
+                        <span class="icon">
+                            <ion-icon name="log-out-outline"></ion-icon>
+                        </span>
+                        <span class="title">Sign Out</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
---
+++ app/views/pages/Lyrics.php
@@ -0,0 +1,220 @@
+<?php  include './assets/include/header.php'  ?>
+
+<!--=========================Main====================-->
+<div class="main">
+    <div class="topbar">
+        <div class="toggle">
+            <ion-icon name="menu-outline"></ion-icon>
+        </div>
+
+        <button class="text-white fs-2 btn">
+            <ion-icon name="notifications-outline"></ion-icon>
+            <!-- <img src="assets/imgs/customer01.jpg" alt="" /> -->
+        </button>
+
+    </div>
+
+    <!--=========================Modal====================-->
+
+    <div class="button-add-student float-end me-4">
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal"
+            data-bs-whatever="@mdo">Add Lyrics</button>
+
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Add Lyrics</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <form method="POST" action="./traitement/addcategory.php" enctype="multipart/form-data">
+                            <div class="mb-3">
+                                <label for="Lyrics" class="col-form-label">Lyrics:</label>
+                                <input type="text" class="form-control" name="Lyrics">
+                            </div>
+                            <div class="mb-3">
+                                <label for="imgLyrics" class="col-form-label">Image:</label>
+                                <input type="file" class="form-control" name="imgLyrics">
+                            </div>
+                            <div class="mb-3">
+                                <label for="dateLyrics" class="col-form-label">Image:</label>
+                                <input type="date" class="form-control" name="dateLyrics">
+                            </div>
+                            <div class="mb-3">
+                                <label for="StatusLyrics" class="col-form-label">Status:</label>
+                                <input type="text" class="form-control" name="StatusLyrics">
+                            </div>
+
+
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                <button type="submit" name="submit" class="btn btn-primary">Add Lyrics</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- ================ Order Details List ================= -->
+    <div class="details">
+        <div class="recentOrders">
+            <div class="cardHeader">
+                <h2>Lyrics</h2>
+                <!-- <a href="#" class="btn">View All</a> -->
+            </div>
+
+            <table>
+                <thead>
+                    <tr>
+                        <td>Name</td>
+                        <td>Image</td>
+                        <td>Date</td>
+                        <td>User</td>
+                        <td>Status</td>
+                        <td>Action</td>
+                    </tr>
+                </thead>
+
+                <tbody>
+
+                    <tr>
+                        <td>Addidas Shoes</td>
+                        <td>$620</td>
+                        <td>Due</td>
+                        <td>Due</td>
+                        <td><span class="status inProgress">In Progress</span></td>
+                        <td> <a href=""><i class=" btn btn-primary far fa-pen"></i></a>
+                            <a href=" "><i class="btn btn-danger far fa-trash"></i></a>
+
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>Star Refrigerator</td>
+                        <td>$1200</td>
+                        <td>Paid</td>
+                        <td>Due</td>
+                        <td><span class="status delivered">Delivered</span></td>
+                        <td> <a href=""><i class=" btn btn-primary far fa-pen"></i></a>
+                            <a href=" "><i class="btn btn-danger far fa-trash"></i></a>
+
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>Dell Laptop</td>
+                        <td>$110</td>
+                        <td>Due</td>
+                        <td>Due</td>
+                        <td><span class="status pending">Pending</span></td>
+                        <td> <a href=""><i class=" btn btn-primary far fa-pen"></i></a>
+                            <a href=" "><i class="btn btn-danger far fa-trash"></i></a>
+
+                        </td>
+                    </tr>
+
+
+                </tbody>
+            </table>
+        </div>
+
+        <!-- ================= New Customers ================ -->
+        <div class="recentCustomers">
+            <div class="cardHeader">
+                <h2>Lyrics </h2>
+            </div>
+
+            <table>
+                <tr>
+                    <td width="60px">
+                        <div class="imgBx">
+                            <img src="assets/imgs/customer02.jpg" alt="" />
+                        </div>
+                    </td>
+                    <td>
+                        <h4>
+                            Song<br />
+                            <span>Italy</span>
+                        </h4>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td width="60px">
+                        <div class="imgBx">
+                            <img src="assets/imgs/customer01.jpg" alt="" />
+                        </div>
+                    </td>
+                    <td>
+                        <h4>
+                            Song <br />
+                            <span>India</span>
+                        </h4>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td width="60px">
+                        <div class="imgBx">
+                            <img src="assets/imgs/customer02.jpg" alt="" />
+                        </div>
+                    </td>
+                    <td>
+                        <h4>
+                            Song <br />
+                            <span>Italy</span>
+                        </h4>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td width="60px">
+                        <div class="imgBx">
+                            <img src="assets/imgs/customer01.jpg" alt="" />
+                        </div>
+                    </td>
+                    <td>
+                        <h4>
+                            Song <br />
+                            <span>India</span>
+                        </h4>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td width="60px">
+                        <div class="imgBx">
+                            <img src="assets/imgs/customer02.jpg" alt="" />
+                        </div>
+                    </td>
+                    <td>
+                        <h4>
+                            Song <br />
+                            <span>Italy</span>
+                        </h4>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td width="60px">
+                        <div class="imgBx">
+                            <img src="assets/imgs/customer01.jpg" alt="" />
+                        </div>
+                    </td>
+                    <td>
+                        <h4>
+                            Song <br />
+                            <span>India</span>
+                        </h4>
+                    </td>
+                </tr>
+
+            </table>
+        </div>
+    </div>
+</div>
+</div>
+
+<?php include './assets/include/footer.php' ?>
