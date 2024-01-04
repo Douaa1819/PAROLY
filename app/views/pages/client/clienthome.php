5e85cf3d9a4ea9aeff529bc9f3b5dff8eb095b15
Aya <ayaelrhayour123@gmail.com>
2024-01-04 12:11:38 +0100

    icons no show

--- app/views/pages/client/clienthome.php
+++ app/views/pages/client/clienthome.php
@@ -2,15 +2,6 @@
 <?php require APPROOT . '/views/inc/Clientheader.php'; ?>
 <?php require APPROOT . '/views/inc/ClientSideBar.php'; ?>
 
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-
 <body>
   <div class="main">
     <div class="topbar">
